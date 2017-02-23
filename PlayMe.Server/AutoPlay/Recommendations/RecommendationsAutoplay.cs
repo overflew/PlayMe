b3d0004ccbb298e394f0bbc2130b0d06307b3aed
overflew <gareth.bradley.nz@gmail.com>
2017-02-23 18:51:03 +1300

    Setup multiple random seeds

--- PlayMe.Server/AutoPlay/Recommendations/RecommendationsAutoplay.cs
+++ PlayMe.Server/AutoPlay/Recommendations/RecommendationsAutoplay.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PlayMe.Common.Model;
+using PlayMe.Common.Util;
 using PlayMe.Data;
 using PlayMe.Data.Mongo;
 using PlayMe.Server.Providers.NewSpotifyProvider;
@@ -20,6 +21,8 @@ namespace PlayMe.Server.AutoPlay.Recommendations
         private readonly Random _random;
 
         const string AutoplayDisplayName = "Autoplay - Recommendations";
+
+        private const int SEED_TRACKS = 30;
 
         // TODO: Move this out to external config. It's required in searches in order to populate 'IsPlayable' on tracks
         private const string LOCAL_MARKET = "NZ";
@@ -41,16 +44,12 @@ namespace PlayMe.Server.AutoPlay.Recommendations
         public QueuedTrack FindTrack()
         {
             // -- Pick a random seed track
-
-            var recentTracks = PickSeedTracks();
-
-            var rand = _random.Next(recentTracks.Count());
-            var seedTrack = recentTracks[rand];
+            var seedTracks = PickRandomSeedTracks();
 
             // -- Grab recommendations
             var client = _spotify.GetClient();
             var recommendations = client.GetRecommendations(
-                trackSeed: new List<string> {seedTrack.Track.Link}, 
+                trackSeed: seedTracks.Select(s => s.Track.Link).ToList(), 
                 market: LOCAL_MARKET);
 
             var pickIndex = _random.Next(recommendations.Tracks.Count);
@@ -62,22 +61,38 @@ namespace PlayMe.Server.AutoPlay.Recommendations
             // -- Map it to the business models
             var mappedTrack = _trackMapper.Map(chosenTrackFull, AutoplayDisplayName, true, true);
 
+            var seedString = string.Join(",", seedTracks.Select(s => $"{s.Track.Artists.First().Name} - {s.Track.Name}"));
             return new QueuedTrack()
             {
                 Track = mappedTrack,
 
                 // Important: Tag the source of this autoplay variant
                 User = AutoplayDisplayName,
-                Reason = $"Seed: {seedTrack.Track.Artists.First().Name} - {seedTrack.Track.Name}"
+                Reason = $"Seed: {seedString}"
             };
         }
 
-        private IList<QueuedTrack> PickSeedTracks()
+        private IList<QueuedTrack> PickRandomSeedTracks()
+        {
+            var recentTracks = PickSeedableTracksFromHistory();
+
+            recentTracks.Shuffle();
+
+            var result = new List<QueuedTrack>();
+            var rnd = _random.Next(2);
+            var tracksToPick = rnd == 0 
+                ? 1 // 50% chance of just using one
+                : _random.Next(2, 5); // Or randomly use more
+
+            return recentTracks.Take(tracksToPick).ToList();
+        }
+
+        private IList<QueuedTrack> PickSeedableTracksFromHistory()
         {
             var x = _queuedTrackDataService.GetAll()
                 .Where(t => t.Likes.Any())
                 .OrderByDescending(t => t.StartedPlayingDateTime)
-                .Take(10)
+                .Take(SEED_TRACKS)
                 .ToList();
 
             return x;
